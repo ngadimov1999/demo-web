39303b3e831e60f4b81756f5b7e98f464cf7fdef
nabi <ngadimov1999@gmail.com>
2020-09-10 20:52:04 +0500

    lesson one1

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,9 @@
 package com.example.demoweb.controller;
 
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,14 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
+    @Autowired
+    PostService postsService;
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        postsService.listAllPosts();
+        model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
+        return "list";
     }
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
     }
+
 }
---
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -0,0 +1,16 @@
+package com.example.demoweb.service;
+
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Service
+public class PostService {
+    public ArrayList<Post> listAllPosts(){
+        return new ArrayList<Post> (Arrays.asList(new Post("Ольгу Бузову повесили на сцене", 5000000),
+                new Post("Дима Билан скончался от передоза", 25000),
+                new Post("Куда пропал Рома Жёлудь?", 3000)));
+    }
+}
