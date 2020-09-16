df686bf659e59ecab52a96c39f6125c0e3dc4b1e
nabi <ngadimov1999@gmail.com>
2020-09-17 00:12:26 +0500

    УБЕЙТЕ МЕНЯ!!!!!!!!!!!!!

--- src/main/java/com/example/demoweb/controller/PostsCreateController.java
+++ src/main/java/com/example/demoweb/controller/PostsCreateController.java
@@ -4,26 +4,18 @@ import com.example.demoweb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class PostsViewController {
+public class PostsCreateController {
     @Autowired
     PostService postsService;
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list(Model model) {
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
         model.addAttribute("appName", "Моё супер приложение");
         model.addAttribute("posts", postsService.listAllPosts());
-        return "list";
+        return "create";
     }
-    @ResponseBody
-    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
-    public String single(@PathVariable("id") Long id) {
-        return "Здесь будет страница поста №" + id;
-    }
-
 }
